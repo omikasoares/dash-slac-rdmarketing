43b973fd7a2fc838b6575d45a52c0ae7fdce804e
Mika Soares <contato.mikasoares@gmail.com>
2026-07-13 08:41:21 -0300

    Preenche nome real do service no deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,4 @@ git pull origin main
 
 docker build -t dash-slac-rdmarketing:latest .
 
-# Troque STACK_NOME_SERVICO pelo nome real depois de criar a stack no Portainer
-# (descubra com: docker service ls | grep dash-slac)
-docker service update --force STACK_NOME_SERVICO
+docker service update --force dash-slac-rdmarketing_dash-slac-rdmarketing
